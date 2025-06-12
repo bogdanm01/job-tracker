020c22fb70cd19823dee229c6170b0c6d7dda1a5
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-06-12 21:39:49 +0200

    Update header, add notification button and dark mode switch button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
 import { useLocation } from "react-router-dom";
+import { Button } from "@heroui/button";
 
-const Header = ({ children }: any) => {
+import { BellIcon, MoonIcon } from "@heroicons/react/24/solid";
+
+const Header = () => {
   const location = useLocation();
 
+  const map: Record<string, string> = {
+    "/tracking": "Job Tracking",
+  };
+
   return (
-    <div className="col-start-2 col-end-2 border-b-1 border-gray-100 flex items-center px-6">
-      {children}
+    <div className="col-start-2 col-end-2 border-b-1 border-gray-100 flex items-center px-6 w-full justify-between">
+      <h1 className="font-medium text-gray-900 text-lg">
+        {map[location.pathname]}
+      </h1>
+      <div className="flex gap-1">
+        <Button isIconOnly variant="light" className="text-gray-600">
+          <BellIcon className="size-5" />
+        </Button>
+        <Button isIconOnly variant="light" className="text-gray-600">
+          <MoonIcon className="size-5" />
+        </Button>
+      </div>
     </div>
   );
 };
