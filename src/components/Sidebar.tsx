2b91706138e91c6a94fc39e41977bef6c5b7c79e
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-06-14 18:43:47 +0200

    Remove unused code

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,11 +5,9 @@ import {
   DropdownMenu,
 } from "@heroui/dropdown";
 import { User } from "@heroui/user";
-
 import {
   BriefcaseIcon,
   CubeIcon,
-  DocumentIcon,
   ChartPieIcon,
   FolderIcon,
   SparklesIcon,
@@ -71,63 +69,6 @@ const Sidebar = () => {
           linkTo={"cover"}
           text="AI Cover Letter"
         />
-
-        {/* <div className="flex w-full gap-2 px-4 py-2 rounded-md cursor-pointer hover:bg-gray-100 items-center transition-all duration-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776"
-            />
-          </svg>
-          <p className="text-sm">Documents</p>
-        </div>
-        <div className="flex w-full gap-2 px-4 py-2 rounded-lg cursor-pointer hover:bg-gray-100 items-center transition-all duration-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
-            />
-          </svg>
-          <p className="text-sm">Reporting</p>
-        </div>
-        <div className="flex w-full gap-2 px-4 py-2 rounded-lg cursor-pointer hover:bg-gray-100 items-center transition-all duration-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
-            />
-          </svg>
-          <p className="text-sm">AI Cover Letter</p>
-        </div> */}
       </nav>
       <div className="mt-auto">
         <Dropdown placement="bottom-start">
